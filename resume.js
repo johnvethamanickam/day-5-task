66d0cd6303a3e599adb4aef13e1a8e6782075b42
johnvethamanickam <125032599+johnvethamanickam@users.noreply.github.com>
2023-07-13 17:16:01 +0530

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,51 @@
+let myResume={
+    "basics": {
+        "id":{
+        "name": "JOHN VETHAMANICKAM.V",
+        "email": "johnny.vjohn82@gmail.com",
+        "phone:8248383283,
+        },
+      "location": {
+        "address": "13/53, V.O.C Nagar, 2nd Street",
+        "postalCode": 628501,
+        "city": "Kovilpatti",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "linkedin": "https://www.linkedin.com/in/johnny.vjohn/",
+          "github":"https://github.com/johnvethamanickam"
+        }
+      ]
+    },
+    "certificatons": [
+        {
+          "name": "Certified Ethical Hacker(CEH V11)",
+          "id": "ECC0241576893",
+          "issueDate": "25-08-2022",
+          "expiryDate": "24-08-2025",
+        },
+      ],
+
+    "skills": [
+      {
+        "name": "C,Python,Javascript,linux,NMAP,NETCAT,Wireshark,Burpsuit,OSINT,OWASP Top 10,Knowledge about Embedded devices",
+        "level": "intermediate",
+        "project": [
+          "Finger Print Voting System Using RaspberryPI"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Coding , Playing games, Playing CTF, watching Youtube",
+      }
+    ]
+  }
+  console.log(myResume);
